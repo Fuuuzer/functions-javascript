986838739784e133c66f741e79f1bdc67bac34e8
Fuzer <fuuzer_@hotmail.com>
2021-11-10 19:33:27 -0300

    functions javascript - DOM

---
+++ functions javascript - DOM/js/script.js
@@ -0,0 +1,10 @@
+/* Scripts do projeto DOM - HTML e JavaScript
+Autor: Fuzer
+data: 11/21
+Versão: 1
+*/
+
+// Mudar valores com o .value
+function mudarTexto() {
+ document.getElementById('texto').value = "Novo Texto"
+}
